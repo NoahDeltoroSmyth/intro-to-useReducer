d4b702b53a57721216c6c77b7b6c7a90fc6c0551
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-14 12:52:50 -0800

    counters increment and decrement

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,41 +1,58 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 
 const pinkRGB = `rgb(236, 72, 153)`
 
+const initialState = { count: 0 }
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case 'increment': {
+      return { count: state.count + 1 }
+    }
+    case 'decrement': {
+      return { count: state.count - 1 }
+    }
+    default: {
+      throw new Error(`Unknown action: ${action.type}`)
+    }
+  }
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   const [currentColor, setCurrentColor] = useState(pinkRGB)
+  const [state, dispatch] = useReducer(counterReducer, initialState)
 
-  useEffect(() => {
-    if (count === 0) {
-      setCurrentColor(pinkRGB)
-    }
+  // useEffect(() => {
+  //   if (count === 0) {
+  //     setCurrentColor(pinkRGB)
+  //   }
 
-    if (count > 0) {
-      setCurrentColor(`rgb(52, 211, 153)`)
-    }
+  //   if (count > 0) {
+  //     setCurrentColor(`rgb(52, 211, 153)`)
+  //   }
 
-    if (count < 0) {
-      setCurrentColor(`rgb(239, 68, 68)`)
-    }
-  }, [count])
+  //   if (count < 0) {
+  //     setCurrentColor(`rgb(239, 68, 68)`)
+  //   }
+  // }, [count])
 
   const increment = () => {
-    setCount((prevState) => prevState + 1)
+    dispatch({ type: 'increment' })
   }
 
   const decrement = () => {
-    setCount((prevState) => prevState - 1)
+    dispatch({ type: 'decrement' })
   }
 
   const reset = () => {
-    setCount(0)
+    // setCount(0)
   }
 
   return (
     <main className="bg-black bg-opacity-90 min-h-screen flex flex-col items-center justify-center text-4xl text-pink-500">
       <h1 className="mb-5" style={{ color: currentColor }}>
-        {count}
+        {state.count}
       </h1>
       <div className="flex w-1/2 justify-around">
         <button
